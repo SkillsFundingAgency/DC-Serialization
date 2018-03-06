6cdcf2874c90f010e9bf4b503a425632c4eaf25c
Mike Childs <mike.childs@education.gov.uk>
2018-03-06 14:27:18 +0000

    Moved Solution to correct level, added Serialization Service Interface and Project

---
+++ src/ESFA.DC.Serialization.Interfaces/ISerializationService.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace ESFA.DC.Serialization.Interfaces
+{
+    public interface ISerializationService
+    {
+        string Serialize<T>(T objectToSerialize);
+
+        void Serialize<T>(T objectToSerialize, Stream stream);
+
+        T Deserialize<T>(string serializedObject);
+
+        T Deserialize<T>(Stream stream);
+    }
+}
