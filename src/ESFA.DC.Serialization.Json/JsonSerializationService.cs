712a170fbae1fa98e88741a4fd3f5c9eab26ebab
Mike Childs <mike.childs@education.gov.uk>
2018-03-07 12:20:32 +0000

    Json Serialization Project and Unit Tests added

---
+++ src/ESFA.DC.Serialization.Json/JsonSerializationService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using ESFA.DC.Serialization.Interfaces;
+using Newtonsoft.Json;
+
+namespace ESFA.DC.Serialization.Json
+{
+    public class JsonSerializationService : ISerializationService
+    {
+        public T Deserialize<T>(string serializedObject)
+        {
+            if (string.IsNullOrWhiteSpace(serializedObject))
+            {
+                throw new ArgumentNullException("Serialized Object string must not be null or whitespace.");
+            }
+
+            return JsonConvert.DeserializeObject<T>(serializedObject);
+        }
+
+        public T Deserialize<T>(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException("Stream must be initialized.");
+            }
+
+            using (var streamReader = new StreamReader(stream))
+            {
+                using (var jsonTextReader = new JsonTextReader(streamReader))
+                {
+                    return new JsonSerializer().Deserialize<T>(jsonTextReader);
+                }
+            }
+        }
+
+        public string Serialize<T>(T objectToSerialize)
+        {
+            if (objectToSerialize == null)
+            {
+                throw new ArgumentNullException("Object To Serialize must not be Null.");
+            }
+
+            return JsonConvert.SerializeObject(objectToSerialize);
+        }
+
+        public void Serialize<T>(T objectToSerialize, Stream stream)
+        {
+            if (objectToSerialize == null)
+            {
+                throw new ArgumentNullException("Object To Serialize must not be Null.");
+            }
+
+            if (stream == null)
+            {
+                throw new ArgumentNullException("Stream must be initialized.");
+            }
+
+            var streamWriter = new StreamWriter(stream);
+
+            var jsonTextWriter = new JsonTextWriter(streamWriter);
+            
+            new JsonSerializer().Serialize(jsonTextWriter, objectToSerialize);
+
+            jsonTextWriter.Flush();
+        }
+    }
+}
